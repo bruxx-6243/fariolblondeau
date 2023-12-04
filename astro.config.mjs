576ad9831df0ab97afb85545121e8e7e662b7f78
brx-nbif <blondeau.nbif@mail.ru>
2023-12-04 16:39:23 +0300

    Refactor: Removing Vercel/serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,19 +10,10 @@ export default defineConfig({
     react(),
     tailwind(),
     compress({
-      css: true,
-      html: true,
-      js: true,
-      img: true,
       imgAttributes: {
         decoding: "async",
         loading: "lazy",
       },
-      icons: false,
     }),
   ],
-  output: "hybrid",
-  adapter: vercel({
-    imageService: true,
-  }),
 });
