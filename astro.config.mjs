495fb7da982a96761690172b30d1982cf6dc2588
brx-nbif <blondeau.nbif@mail.ru>
2023-12-05 00:24:59 +0300

    Add: Vercel/serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,26 +3,23 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
-
 import partytown from "@astrojs/partytown";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fariolblondeau.vercel.app/",
-  integrations: [
-    react(),
-    tailwind(),
-    compress({
-      imgAttributes: {
-        decoding: "async",
-        loading: "lazy",
-      },
-    }),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [react(), tailwind(), compress({
+    imgAttributes: {
+      decoding: "async",
+      loading: "lazy"
+    }
+  }), sitemap(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })],
+  output: "server",
+  adapter: vercel()
 });
