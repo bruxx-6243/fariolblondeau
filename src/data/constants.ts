ade79de98096bf3b831dd1fc0893b8bbcd5b414d
brx-nbif <blondeau.nbif@mail.ru>
2023-12-18 19:10:00 +0300

    [Add]: Adding the constants folder

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -26,7 +26,7 @@ export const socials = [
   {
     name: "Github",
     icon: Github,
-    url: `https://github.com/${Author.github}/${Author.github}/issues/1#issue-2046927702`,
+    url: `https://github.com/${Author.github}`,
   },
   {
     name: "Twitter",
