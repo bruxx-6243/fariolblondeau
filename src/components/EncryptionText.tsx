fb1f9d69ce804e4fb7e7dec503d6cdea2c080e2e
brx-nbif <blondeau.nbif@mail.ru>
2024-11-18 12:12:27 +0100

    feat: update my website

---
+++ src/components/EncryptionText.tsx
@@ -0,0 +1,87 @@
+import { useRef, useState } from "react";
+import { motion } from "framer-motion";
+
+export const EncryptText = ({ targetText }: { targetText: string }) => {
+  return (
+    <div className="grid min-h-[200px] place-content-center bg-neutral-900 p-4">
+      <EncryptButton targetText={targetText} />
+    </div>
+  );
+};
+
+const CYCLES_PER_LETTER = 2;
+const SHUFFLE_TIME = 50;
+
+const CHARS = "!@#$%^&*():{};|,.<>/?";
+
+const EncryptButton = ({ targetText }: { targetText: string }) => {
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const [text, setText] = useState(targetText);
+
+  const scramble = () => {
+    let pos = 0;
+
+    intervalRef.current = setInterval(() => {
+      const scrambled = targetText
+        .split("")
+        .map((char, index) => {
+          if (pos / CYCLES_PER_LETTER > index) {
+            return char;
+          }
+
+          const randomCharIndex = Math.floor(Math.random() * CHARS.length);
+          const randomChar = CHARS[randomCharIndex];
+
+          return randomChar;
+        })
+        .join("");
+
+      setText(scrambled);
+      pos++;
+
+      if (pos >= targetText.length * CYCLES_PER_LETTER) {
+        stopScramble();
+      }
+    }, SHUFFLE_TIME);
+  };
+
+  const stopScramble = () => {
+    clearInterval(intervalRef.current || undefined);
+
+    setText(targetText);
+  };
+
+  return (
+    <motion.div
+      whileHover={{
+        scale: 1.025,
+      }}
+      whileTap={{
+        scale: 0.975,
+      }}
+      onMouseEnter={scramble}
+      onMouseLeave={stopScramble}
+      className="group relative overflow-hidden rounded-lg border-[1px] border-neutral-500 bg-neutral-700 px-4 py-2 font-mono font-medium uppercase text-neutral-300 transition-colors hover:text-indigo-300"
+    >
+      <div className="relative z-10 flex items-center gap-2">
+        <span>{text}</span>
+      </div>
+      <motion.span
+        initial={{
+          y: "100%",
+        }}
+        animate={{
+          y: "-100%",
+        }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "mirror",
+          duration: 1,
+          ease: "linear",
+        }}
+        className="duration-300 absolute inset-0 z-0 scale-125 bg-gradient-to-t from-indigo-400/0 from-40% via-indigo-400/100 to-indigo-400/0 to-60% opacity-0 transition-opacity group-hover:opacity-100"
+      />
+    </motion.div>
+  );
+};
