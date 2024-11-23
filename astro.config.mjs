ca88cbcae9cc156e12c7460c239b8ba3cea88ae8
brx-nbif <blondeau.nbif@mail.ru>
2024-11-24 02:57:08 +0530

    chore: change the vercel output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
