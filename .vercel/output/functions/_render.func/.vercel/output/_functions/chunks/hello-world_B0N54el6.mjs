1ec410c52edeedf8314cb3a820e02ec81db1d71d
brx-nbif <blondeau.nbif@mail.ru>
2024-11-24 03:28:18 +0530

    feat: add output server with node env

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/hello-world_B0N54el6.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./hello-world_CXvZDx5f.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
